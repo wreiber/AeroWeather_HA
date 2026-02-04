e81d11502397250ef3dcc0c0339c824e14596180
Will Reiber <117045451+wreiber@users.noreply.github.com>
2026-02-03 22:05:21 -0500

    Update const.py

--- custom_components/aeroweather/const.py
+++ custom_components/aeroweather/const.py
@@ -5,3 +5,14 @@ CONF_SCAN_INTERVAL = "scan_interval"
 
 DEFAULT_SCAN_INTERVAL = 300  # seconds
 API_BASE = "https://aviationweather.gov/api/data"
+
+DOMAIN = "aeroweather"
+
+CONF_ICAOS = "icaos"
+CONF_SCAN_INTERVAL = "scan_interval"
+
+DEFAULT_NOTAM_SCAN_INTERVAL = 15  # minutes
+
+NOTAM_SEVERITY_GREEN = "GREEN"
+NOTAM_SEVERITY_YELLOW = "YELLOW"
+NOTAM_SEVERITY_RED = "RED"
